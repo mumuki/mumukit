9bb5bc38da46ab18dbd8f5f909bd71ba6eee312d
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-01-03 20:45:21 -0300

    Adding stubs dir

--- lib/mumukit.rb
+++ lib/mumukit.rb
@@ -5,7 +5,7 @@ require_relative 'mumukit/version'
 require_relative 'mumukit/command_line_test_runner'
 require_relative 'mumukit/file_test_compiler'
 
-require_relative 'test_compiler'
-require_relative 'test_runner'
+require_relative 'stubs/test_compiler'
+require_relative 'stubs/test_runner'
 
 require_relative 'mumukit/test_server_app'
