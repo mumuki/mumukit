5f4e4658b8a86a5301ad7801037bc1312e2b2ae8
mgarciaisaia <mgarciaisaia+github@gmail.com>
2015-04-18 16:34:03 -0300

    Configurable source file name in compiler
    `gcc` _kind of needs_ the source file to have a `.c` extension,
    so we'd better let each server to define it's own temp files names

--- lib/mumukit/file_test_compiler.rb
+++ lib/mumukit/file_test_compiler.rb
@@ -2,8 +2,12 @@ require 'tempfile'
 
 module Mumukit::FileTestCompiler
 
+  def create_tempfile
+    Tempfile.new('mumuki.compile')
+  end
+
   def create_compilation_file!(test, extra, content)
-    file = Tempfile.new('mumuki.compile')
+    file = create_tempfile
     file.write(compile(test, extra, content))
     file.close
     file
