ab182a898ba05a5d249455c053f21b9ae243c36c
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-09 12:45:25 -0300

    Adding limit

--- lib/mumukit/with_command_line.rb
+++ lib/mumukit/with_command_line.rb
@@ -1,7 +1,21 @@
 module Mumukit
   module WithCommandLine
+    def command_size_limit
+      1024
+    end
+
+    def command_time_limit
+      4
+    end
+
     def run_command(command)
-      [%x{#{command}}, $?.success? ? :passed : :failed]
+      out = %x{limit #{command_size_limit} #{command_time_limit} #{command}}
+      case $?.exitstatus
+        when 0 then [out, :passed]
+        when 152 then ["Time exceeded #{command_time_limit}", :failed]
+        when 130..139 then ['Memory exceeded', :failed]
+        else [out, :failed]
+      end
     end
   end
 end
