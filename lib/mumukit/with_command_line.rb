dfacd0f8cfa2cf362172f1fd1778735992bf9e40
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-09 14:18:35 -0300

    Adding tests

--- lib/mumukit/with_command_line.rb
+++ lib/mumukit/with_command_line.rb
@@ -8,12 +8,19 @@ module Mumukit
       4
     end
 
+    def limit_command
+      'limit'
+    end
+
     def run_command(command)
-      out = %x{limit #{command_size_limit} #{command_time_limit} #{command}}
+      out = %x{#{limit_command} #{command_size_limit} #{command_time_limit} $(cat <<EOLIMIT
+#{command}
+EOLIMIT
+)}
       case $?.exitstatus
         when 0 then [out, :passed]
-        when 152 then ["Time exceeded #{command_time_limit}", :failed]
-        when 130..139 then ['Memory exceeded', :failed]
+        when 152 then ["Time exceeded #{command_time_limit}", :aborted]
+        when 130..139 then ['Memory exceeded', :aborted]
         else [out, :failed]
       end
     end
