0c60de5d5a1f47f147cd3c3c3ba94b64f292eea4
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-17 22:40:02 -0300

    Putting translates in standard locale dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,5 @@ require 'rspec'
 require_relative '../lib/mumukit'
 
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
-I18n.load_path = Dir['lib/locales/*.yml']
+I18n.load_path = Dir['locales/*.yml']
 I18n.backend.load_translations
