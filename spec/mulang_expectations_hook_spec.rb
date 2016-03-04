564a95129df17f7807db6184708fa3d0e14f747e
Federico Aloi <federico.aloi@gmail.com>
2016-03-04 17:08:01 -0300

    Generalized ExpectationsHook deletion

--- spec/mulang_expectations_hook_spec.rb
+++ spec/mulang_expectations_hook_spec.rb
@@ -4,25 +4,26 @@ describe Mumukit::Templates::MulangExpectationsHook do
   let(:content) { 'x = 1' }
   let(:expectations) { [{ subject: ['x'], transitive: false, negated: false, verb: 'uses', object: { tag: 'Anyone', contents: [] } }] }
 
+  after do
+    drop_hook ExpectationsHook
+  end
+
+  let(:hook) { ExpectationsHook.new('mulang_path' => 'mulang') }
+
   context '#run!' do
     let(:request) { { content: content, expectations: expectations } }
 
     context 'when language is not defined' do
-      class WithoutLanguage < Mumukit::Templates::MulangExpectationsHook
+      before do
+        class ExpectationsHook < Mumukit::Templates::MulangExpectationsHook
+        end
       end
 
-      let(:hook) { WithoutLanguage.new('mulang_path' => 'mulang') }
       it { expect { hook.run! request }.to raise_error(Exception, 'You need to implement method language') }
     end
   end
 
   context '#mulang_json' do
-    after do
-      drop_hook ExpectationsHook
-    end
-
-    let(:hook) { ExpectationsHook.new('mulang_path' => 'mulang') }
-
     context 'with defaults' do
       before do
         class ExpectationsHook < Mumukit::Templates::MulangExpectationsHook
