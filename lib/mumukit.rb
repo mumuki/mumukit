a23d7127e343630a3b3bf6dae474bd4cf067baa4
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-07-07 16:36:06 -0300

    Removing mulang duplicated config
    It was already a config file property

--- lib/mumukit.rb
+++ lib/mumukit.rb
@@ -51,7 +51,6 @@ Mumukit.configure_defaults do |config|
   config.limit_script = File.join(pwd, '..', 'bin', 'limit')
   config.content_type = :plain
   config.structured = false
-  config.mulang_path = 'mulang'
   config.command_time_limit = 4
   config.command_size_limit = 1024
 end
