026dfe8c3d447ac515f1ff33e0a96ede498d2f37
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-07-07 22:50:52 -0300

    Doing some json transforms

--- lib/mumukit/templates/mulang_expectations_hook.rb
+++ lib/mumukit/templates/mulang_expectations_hook.rb
@@ -5,31 +5,32 @@ module Mumukit
     required :language, 'You have to provide a Mulang-compatible language in order to use this hook'
 
     def compile(request)
-      request
-    end
-
-    def run!(request)
-      result = run_command "#{mulang_path} '#{mulang_input(request[:content], request[:expectations]).to_json}'"
-      make_response JSON.parse(result[0])
-    end
-
-    def transform_content(content)
-      content
-    end
-
-    def transform_expectations(expectations)
-      expectations
-    end
-
-    def mulang_input(content, expectations)
       {
-        expectations: transform_expectations(expectations),
-        code: { content: transform_content(content), language: language}
+          expectations: request[:expectations].map { |it| compile_expectation(it.deep_symbolize_keys) },
+          code: {
+              content: compile_content(request[:content]),
+              language: language}
       }
     end
 
-    def make_response(mulang_output)
-      mulang_output['results']
+    def compile_content(content)
+      content
+    end
+
+    def compile_expectation(expectation)
+      (expectation[:verb].present? ? {tag: :Advanced} : {tag: :Basic}).merge(expectation)
+    end
+
+    def run!(request)
+      result = run_command "#{mulang_path} '#{request.to_json}'"
+      parse_response JSON.parse(result[0])
+    end
+
+    def parse_response(response)
+      response['results'].map do |it|
+        {result: it['result'],
+         expectation: it['expectation'].deep_symbolize_keys}
+      end
     end
 
     def self.include_smells(value=true)
