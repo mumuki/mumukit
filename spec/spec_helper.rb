2be63aadeff38755571df2a1f24286de73e708d6
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-07-07 22:43:06 -0300

    Making test reproducible

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,13 @@ require_relative '../lib/mumukit'
 def drop_hook(hook_class)
   Object.send :remove_const, hook_class.name.to_sym
 end
+
+Mumukit.runner_name = 'demo'
+Mumukit.configure do |c|
+  c.docker_image = 'ubuntu'
+end
+
+class File
+  def unlink
+  end
+end
