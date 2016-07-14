f7b2cb76866d22ae127b15bb97de7a7c2284df92
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-07-13 21:42:15 -0300

    Forcing UTF8 encoding in json parsing

--- lib/mumukit/json.rb
+++ lib/mumukit/json.rb
@@ -4,6 +4,6 @@ module JSON
   def self.pretty_parse(string)
     parse(string)
   rescue => e
-    raise JSON::ParserError, "Invalid JSON #{string}:\n\t#{e.message}"
+    raise JSON::ParserError, "Invalid JSON #{string}:\n\t#{e.message.force_encoding('utf-8')}"
   end
 end
