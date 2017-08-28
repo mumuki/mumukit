b99e8fd18d15a13f7b318a7da8afbd87ba5d37a6
Julian Berbel Alt <julianberbel@hotmail.com>
2017-08-28 16:28:58 -0300

    Allow mashup to map fields before joining

--- lib/mumukit/templates/with_mashup_file_content.rb
+++ lib/mumukit/templates/with_mashup_file_content.rb
@@ -1,11 +1,15 @@
 module Mumukit
   module Templates::WithMashupFileContent
     def compile_file_content(request)
-      mashup_fields.map { |field| request.public_send field }.join("\n")
+      map_fields(mashup_fields.map { |field| request.public_send field }).join("\n")
     end
 
     def mashup_fields
       raise 'must define mashup fields'
+    end
+
+    def map_fields(fields)
+    	fields
     end
   end
 end
