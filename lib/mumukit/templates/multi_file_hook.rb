5be18dc29815452052d56a54db89e7b9731daf25
Rodrigo Alfonso <rodri042@gmail.com>
2019-01-12 00:06:20 -0300

    Creating has_files? helper

--- lib/mumukit/templates/multi_file_hook.rb
+++ lib/mumukit/templates/multi_file_hook.rb
@@ -3,14 +3,14 @@ module Mumukit
     include Mumukit::Templates::WithMultipleFiles
 
     def compile(request)
-      return super.compile(request) if files_of(request).empty?
+      return super(request) unless has_files?(request)
 
       self.request = request
       compile_file_content(request).map { |filename, content| write_tempfile!(content, filename) }
     end
 
     def run!(files)
-      return super.run!(files) if files_of(request).empty?
+      return super(files) unless has_files?(request)
 
       result, status = run_files!(*files)
       post_process_file(files, cleanup_raw_result(result), status)
--- lib/mumukit/templates/with_multiple_files.rb
+++ lib/mumukit/templates/with_multiple_files.rb
@@ -1,6 +1,10 @@
 module Mumukit::Templates::WithMultipleFiles
   def files_of(request)
     raise 'You need to enable Mumukit.config.multifile first!' unless Mumukit.config.multifile
-    request.content.is_a?(Hash) ? request.content : {}
+    has_files?(request) ? request.content : {}
+  end
+
+  def has_files?(request)
+    request.content.is_a?(Hash)
   end
 end
