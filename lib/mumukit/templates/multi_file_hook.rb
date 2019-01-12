6ec00c5ff9d35cc1df24a65c84b6de578e12f133
Rodrigo Alfonso <rodri042@gmail.com>
2019-01-12 00:01:00 -0300

    It behaves like a single file hook if the content is single file

--- lib/mumukit/templates/multi_file_hook.rb
+++ lib/mumukit/templates/multi_file_hook.rb
@@ -3,11 +3,15 @@ module Mumukit
     include Mumukit::Templates::WithMultipleFiles
 
     def compile(request)
+      return super.compile(request) if files_of(request).empty?
+
       self.request = request
       compile_file_content(request).map { |filename, content| write_tempfile!(content, filename) }
     end
 
     def run!(files)
+      return super.run!(files) if files_of(request).empty?
+
       result, status = run_files!(*files)
       post_process_file(files, cleanup_raw_result(result), status)
     ensure
