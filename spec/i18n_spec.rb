3207115cbbdeb4b374e5682fe18d93884d865dff
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-08-29 19:56:53 -0300

    Adding es-CL locale

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -3,7 +3,8 @@ require_relative 'spec_helper'
 describe 'I18n concern' do
 
   it { expect(I18n.locale_available? :en).to be true }
-  it { expect(I18n.locale_available? :es).to be true }
   it { expect(I18n.locale_available? :pt).to be true }
+  it { expect(I18n.locale_available? :es).to be true }
+  it { expect(I18n.locale_available? 'es-CL').to be true }
 
 end
