0daab876cd3de1c7a1fad0fec575a28de14b2274
Felipe Calvo <felipe@mumuki.org>
2020-11-06 12:59:09 -0300

    Make explainer format-independent

--- lib/mumukit/explainer.rb
+++ lib/mumukit/explainer.rb
@@ -1,11 +1,15 @@
 class Mumukit::Explainer
+  include Mumukit::WithContentType
+
   def explain(content, test_results)
+    content_type.enumerate(explanations(content, test_results))
+  end
+
+  def explanations(content, test_results)
     explain_methods
         .map { |selector, key| eval_explain(selector, key, content, test_results) }
         .compact
         .map { |explain| I18n.t(explain[:key], explain[:binding]) }
-        .map { |it| "* #{it}" }
-        .join("\n")
   end
 
   def eval_explain(selector, key, content, test_results)
